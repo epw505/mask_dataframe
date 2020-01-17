b7f4ef961f6c5f3eea284c7b6fa57fa3ddb38e05
epw505 <edwardpwebter@gmail.com>
2020-01-17 18:52:43 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
-from distutils.core import setup
+import setuptools
 
 import version
 
 
-setup(name="zorro_df", version=version.__version__, packages=["zorro_df"])
+with open("README.md", "r") as read_me:
+    long_description = read_me.read()
+
+setuptools.setup(
+    name="zorro_df", 
+    version=version.__version__,
+    author="Ned Webster",
+    author_email="edwardpwebster@gmail.com",
+    description="Package to mask pd.DataFrame data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/epw505/zorro_df",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Licence :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
