7d628ceefb2eb01c4c0796ac052a83e98cde1020
epw505 <edwardpwebter@gmail.com>
2020-04-08 08:50:18 +0100

    added convert_array_type method"

--- zorro_df/numerical_scalers.py
+++ zorro_df/numerical_scalers.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 
 class Scaler(object):
     """Scaler class containing key functionality for scaling.
@@ -30,6 +30,35 @@ class Scaler(object):
                 raise TypeError("array_like should contain only numeric values")
         
         self.array_like = array_like
+        self.array_type = type(array_like)
+    
+    def convert_array_type(self, array_like, new_type):
+        """Converts an array to a given type.
+
+        The funtion converts an array-like object to either a pandas Series or
+        an numpy ndarray. If neither of those, it defaults to creating a list.
+
+        Parameters
+        ----------
+        array_like : array_like
+            1d numerical array to be converted to given type.
+        new_type : type
+            Given type to convert the data to.
+        
+        Returns
+        -------
+        array_like : array_like
+            1d numerical array after type conversion.
+        
+        """
+
+        if new_type in [list, pd.Series]:
+            array_like = new_type(array_like)
+        
+        elif new_type == np.ndarray:
+            array_like = np.array(array_like)
+        
+        return array_like
 
     def get_min_max_values(self):
         """Calculates the minimum and maximum of a numerical array-like object.
